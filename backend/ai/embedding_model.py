c9017f9fe9b73636334e2d332c1cba9bd07a5252
sora-japan <takayuki555ft@gmail.com>
2026-09-21 00:41:29 +0900

    add: トークン用のファイルを追加、update: トークンを数えられる形に修正

--- backend/ai/embedding_model.py
+++ backend/ai/embedding_model.py
@@ -1,25 +1,46 @@
 import torch
 import torch.nn.functional as F
 from sentence_transformers import SentenceTransformer
+from data.text_extraction import extract_text, normalization_text
+from transformers import AutoTokenizer
+import xml.etree.ElementTree as ET
+import pathlib
+
+BASE_DIR = pathlib.Path(__file__).parent
+XML_DIR = BASE_DIR / "token_file"
 
 device = "mps" if torch.mps.is_available() else "cpu"
-model = SentenceTransformer("cl-nagoya/ruri-v3-310m", device=device)
+model_name = "cl-nagoya/ruri-v3-310m"
+model = SentenceTransformer(model_name, device=device)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-sentences = [
-    "川べりでサーフボードを持った人たちがいます",
-    "サーファーたちが川べりに立っています",
-    "トピック: 瑠璃色のサーファー",
-    "検索クエリ: 瑠璃色はどんな色？",
-    "検索文書: 瑠璃色（るりいろ）は、紫みを帯びた濃い青。名は、半貴石の瑠璃（ラピスラズリ、英: lapis lazuli）による。JIS慣用色名では「こい紫みの青」（略号 dp-pB）と定義している[1][2]。",
-]
+text = ""
+result = []
+for path in XML_DIR.rglob('*xml'):
+    root = ET.parse(path).getroot()
+    main = root.find('LawBody').find('MainProvision')
+    for article in main.findall('.//Article'):
+        text = normalization_text(extract_text(article))
+        length = len(text)
+        article_num = article.get('Num')
+        encode = tokenizer.encode(text)
+        result.append((path.stem, article_num, len(encode), len(encode)/length*100))
 
-embeddings = model.encode(sentences, convert_to_tensor=True)
-print(embeddings.size())
+for r in sorted(result, key=lambda x: x[2]):
+    print(r)
 
-similarities = F.cosine_similarity(
-    embeddings.unsqueeze(0),
-    embeddings.unsqueeze(1),
-    dim=2
-    )
+# print(text)
+# print(len(text))
+# encode = tokenizer.encode(text)
+# print(len(encode))
 
-print(similarities)
+# embeddings = model.encode(text, convert_to_tensor=True)
+# print(embeddings.size())
+# 
+# similarities = F.cosine_similarity(
+#     embeddings.unsqueeze(0),
+#     embeddings.unsqueeze(1),
+#     dim=2
+#     )
+# 
+# print(similarities)
